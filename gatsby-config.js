bfc91a1c97dacbc03d3401196c85bdb4f8d44ef2
Anita Chang <huilong494@gmail.com>
2020-04-18 14:48:27 -0700

    Revert "gitpage"
    This reverts commit 3b2c886c965dad1e792af08d37b5210c209048c9.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  pathPrefix: "/toastmaster_web",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -16,7 +15,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
